8702ce0a03a70edb1130da2d5577be7796a86bcf
Ekisola Babatunji <65820475+DBasic-cmd@users.noreply.github.com>
2024-02-21 10:00:56 +0100

    Add files via upload
    Python assignment 1

---
+++ Python/user_input.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+location = input("Enter your location: ")
+
+u = f'Hello {name}. You are {age} years old and live in {location}'
+
+print(u)
